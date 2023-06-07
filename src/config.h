eb4a7e0f1a8b8ee238c1bdbeb346821177ec20ab
Hamza Anver <hamzaanver1234@gmail.com>
2023-06-07 23:36:38 +0400

    config starting

--- src/config.h
+++ src/config.h
@@ -7,9 +7,15 @@
 
 #define RF95_FREQ 915.0
 
-#define RF95_CS 64 //PF11
-#define RF95_INT 70 //PB11
-#define RF95_RST 68 //PF15
-#define LED 22 //PF10
+#define RF95_CS 64  // PF11
+#define RF95_INT 70 // PB11
+#define RF95_RST 68 // PF15
+#define LED 22      // PF10
+
+#define CUR_I2C_SDA 10 // PF0
+#define CUR_I2C_SCL 11 // PF1
+
+#define HDRM4_DO 1 // PE2
+#define HDRM4_DI 2 // PE3
 
 #endif
